78fda004524dbcfc265999a0fc9d08d0868b0118
Furkan Emin <femincanx@gmail.com>
2023-04-26 17:46:53 +0300

    feat(constants): add colors constant

---
+++ src/constants/colors.ts
@@ -0,0 +1,23 @@
+const greenColors: string[] = ['#1abc9c', '#16a085', '#2ecc71', '#27ae60'];
+
+const yellowOrangeRedColors: string[] = [
+  '#f1c40f',
+  '#f39c12',
+  '#e67e22',
+  '#d35400',
+  '#e74c3c',
+  '#c0392b',
+];
+
+const bluePurpleColors: string[] = [
+  '#3498db',
+  '#2980b9',
+  '#9b59b6',
+  '#8e44ad',
+  '#34495e',
+  '#2c3e50',
+];
+
+const greyColors: string[] = ['#ecf0f1', '#bdc3c7', '#95a5a6', '#7f8c8d'];
+
+export { greenColors, yellowOrangeRedColors, bluePurpleColors, greyColors };
