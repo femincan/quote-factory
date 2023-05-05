6a6f820ad72e9fc54c0da5939ad594091255aa5b
Furkan Emin <femincanx@gmail.com>
2023-05-05 07:35:41 +0300

    feat(style): add fonts and change base layer height

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+        'sans-2': ['Acme', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
