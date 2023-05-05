692c65104d95aaf5c5178bc32c01c6908b812e4d
Furkan Emin <femincanx@gmail.com>
2023-05-05 08:00:33 +0300

    feat(components): remove className prop from Logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,8 @@
 import LogoSvg from '@src/assets/logo.svg';
 
-type LogoProps = {
-  className?: string;
-};
-
-const Logo = ({ className }: LogoProps) => (
-  <div className={`flex items-center justify-center p-3 ${className || ''}`}>
-    <img src={LogoSvg} alt='logo' />
+const Logo = () => (
+  <div className='flex items-center justify-center p-3'>
+    <img src={LogoSvg} alt='' />
   </div>
 );
 
