7115164d7b2181a96842b03ef98f4f22be388311
Furkan Emin <femincanx@gmail.com>
2023-05-05 08:42:44 +0300

    revert(components): remove random color feature related code

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,26 +1,15 @@
 import { ReactNode } from 'react';
-import { motion } from 'framer-motion';
-import { getRandomItem } from '@src/utils';
 
 type ContainerProps = {
-  colorsArray: string[];
   children: ReactNode;
 };
 
-const Container = ({ children, colorsArray }: ContainerProps) => {
-  const bgColor = getRandomItem(colorsArray);
-
-  return (
-    <motion.div
-      className='min-h-[inherit] w-full p-5'
-      initial={{ backgroundColor: colorsArray[0] }}
-      animate={{ backgroundColor: bgColor }}
-    >
-      <div className='mx-auto flex min-h-[inherit] w-full flex-col items-center justify-start gap-10 sm:max-w-7xl'>
-        {children}
-      </div>
-    </motion.div>
-  );
-};
+const Container = ({ children }: ContainerProps) => (
+  <div className='min-h-[inherit] w-full bg-midnight p-5'>
+    <div className='mx-auto flex min-h-[inherit] w-full flex-col items-center justify-start gap-10 sm:max-w-7xl'>
+      {children}
+    </div>
+  </div>
+);
 
 export { Container };
