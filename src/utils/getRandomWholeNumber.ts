982362a40cbb18aca4c7235383601b1c06d04c92
Furkan Emin <femincanx@gmail.com>
2023-05-05 08:37:42 +0300

    revert(utils): remove utilities related to random color feature

--- src/utils/getRandomWholeNumber.ts
+++
@@ -1,4 +0,0 @@
-const getRandomWholeNumber = (min: number, max: number): number =>
-  Math.floor(Math.random() * (max - min) + min);
-
-export { getRandomWholeNumber };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,1 @@
 export * from './fetcher';
-export * from './getRandomWholeNumber';
-export * from './getRandomItem';
