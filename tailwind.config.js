8ed42b8a360d97e1ca11aef2875e16a195e4fa02
Furkan Emin <femincanx@gmail.com>
2023-05-05 18:46:40 +0300

    feat(config): extend theme with new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ export default {
         sans: ['Rubik', ...defaultTheme.fontFamily.sans],
         'sans-2': ['Acme', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        midnight: '#2c3e50',
+        'sun-flower': '#f1c40f',
+        clouds: '#ecf0f1',
+      },
     },
   },
   plugins: [],
