93312a59b8017b6858fb89049efc6b5ad554377c
Furkan Emin <femincanx@gmail.com>
2023-05-05 07:26:08 +0300

    feat(hooks): rename data variable to quote and make it return a single quote

--- src/hooks/useRandomQuote.ts
+++ src/hooks/useRandomQuote.ts
@@ -1,14 +1,30 @@
 import useSWR from 'swr/immutable';
+import { fetcher } from '@src/utils';
+
+type Quote = {
+  content: string;
+  author: string;
+};
 
 const useRandomQuote = () => {
   const { data, error, isLoading, mutate } = useSWR(
-    'https://api.quotable.io/quotes/random'
+    'https://api.quotable.io/quotes/random',
+    fetcher
   );
 
+  let quote: Quote | undefined;
+
+  if (data === undefined) {
+    quote = data;
+  } else {
+    [quote] = data as Quote[];
+  }
+
   return {
-    quote: data,
+    quote,
     error,
     isLoading,
+
     mutate,
   };
 };
