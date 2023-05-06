b5e1dc414ee068308b99d6554138948d32154088
Furkan Emin <femincanx@gmail.com>
2023-05-06 08:33:16 +0300

    feat: add components into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { MotionConfig } from 'framer-motion';
+import { Container, Footer, Logo, QuoteCard } from './components';
+
+const App = () => (
+  <MotionConfig reducedMotion='user'>
+    <Container>
+      <Logo />
+      <QuoteCard />
+      <Footer />
+    </Container>
+  </MotionConfig>
+);
 
 export default App;
