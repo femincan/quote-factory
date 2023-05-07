c7338d78cfa3f535354de18b19e4d4bb23ea7a55
Furkan Emin <femincanx@gmail.com>
2023-05-07 17:52:23 +0300

    feat(components): add animations on quote change

--- src/components/QuoteCard/Quote.tsx
+++ src/components/QuoteCard/Quote.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { tweenTransition } from '@src/constants';
 import { useRandomQuote } from '@src/hooks';
 import { QuoteSkeleton } from './QuoteSkeleton';
 
@@ -5,18 +7,28 @@ const Quote = () => {
   const { quote, isValidating } = useRandomQuote();
 
   return (
-    <div className='flex flex-1 flex-col items-center justify-center gap-5 text-center font-sans-2 text-xl sm:text-2xl'>
-      {isValidating ? (
-        <QuoteSkeleton />
-      ) : (
-        <>
-          <p id='text'>{quote?.content}</p>
-          <p id='author' className='italic'>
-            — {quote?.author}
-          </p>
-        </>
-      )}
-    </div>
+    <AnimatePresence mode='wait'>
+      <div
+        className='flex flex-1 flex-col items-center justify-center gap-5 text-center font-sans-2 text-xl sm:text-2xl'
+        key='quote'
+      >
+        {isValidating ? (
+          <QuoteSkeleton />
+        ) : (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ opacity: tweenTransition }}
+          >
+            <p id='text'>{quote?.content}</p>
+            <p id='author' className='italic'>
+              — {quote?.author}
+            </p>
+          </motion.div>
+        )}
+      </div>
+    </AnimatePresence>
   );
 };
 
--- src/components/QuoteCard/QuoteSkeleton.tsx
+++ src/components/QuoteCard/QuoteSkeleton.tsx
@@ -1,16 +1,29 @@
-const QuoteSkeleton = () => (
-  <div
-    role='status'
-    className='flex w-full animate-pulse flex-col items-center justify-center gap-3.5 opacity-70'
-  >
-    <div className='h-2 w-full max-w-[200px] rounded-full bg-midnight sm:max-w-[360px]' />
-    <div className='h-2 w-full max-w-[260px] rounded-full bg-midnight sm:max-w-none' />
-    <div className='h-2 w-full max-w-[170px] rounded-full bg-midnight sm:max-w-[330px]' />
-    <div className='h-2 w-full max-w-[140px] rounded-full bg-midnight sm:max-w-[300px]' />
-    <div className='h-2 w-full max-w-[200px] rounded-full bg-midnight sm:max-w-[360px]' />
-    <div className='mt-4 h-3.5 w-32 max-w-full rounded-full bg-midnight sm:w-40' />
-    <span className='sr-only'>Loading...</span>
-  </div>
-);
+import { motion, useIsPresent } from 'framer-motion';
+import { tweenTransition } from '@src/constants';
+
+const QuoteSkeleton = () => {
+  const isPresent = useIsPresent();
+
+  return (
+    <motion.div
+      role='status'
+      className={`flex w-full ${
+        isPresent ? 'animate-pulse' : ''
+      } flex-col items-center justify-center gap-3.5`}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 0.7 }}
+      exit={{ opacity: 0 }}
+      transition={{ opacity: tweenTransition }}
+    >
+      <div className='h-2 w-full max-w-[200px] rounded-full bg-midnight sm:max-w-[360px]' />
+      <div className='h-2 w-full max-w-[260px] rounded-full bg-midnight sm:max-w-none' />
+      <div className='h-2 w-full max-w-[170px] rounded-full bg-midnight sm:max-w-[330px]' />
+      <div className='h-2 w-full max-w-[140px] rounded-full bg-midnight sm:max-w-[300px]' />
+      <div className='h-2 w-full max-w-[200px] rounded-full bg-midnight sm:max-w-[360px]' />
+      <div className='mt-4 h-3.5 w-32 max-w-full rounded-full bg-midnight sm:w-40' />
+      <span className='sr-only'>Loading...</span>
+    </motion.div>
+  );
+};
 
 export { QuoteSkeleton };
