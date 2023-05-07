deb00b38c231fb2f21fd7af2c892222d470d0a2f
Furkan Emin <femincanx@gmail.com>
2023-05-07 22:58:22 +0300

    feat(components): align all the text to center

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 };
 
 const Container = ({ children }: ContainerProps) => (
-  <div className='min-h-[inherit] w-full bg-midnight p-5'>
+  <div className='min-h-[inherit] w-full bg-midnight p-5 text-center'>
     <div className='mx-auto flex min-h-[inherit] w-full flex-col items-center justify-start gap-10 sm:max-w-7xl'>
       {children}
     </div>
